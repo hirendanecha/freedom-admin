dfe553cb8870b9d591403ec5fafaa4cd51f3c6c0
AjAy Opash <agh.opash@gmail.com>
2023-08-14 19:27:22 +0530

    Fix user pagination issue and create common component for pagination and also fix some code for user page

---
+++ src/app/@shared/components/pagination/pagination.component.ts
@@ -0,0 +1,84 @@
+import { Component, EventEmitter, Input, Output, SimpleChanges } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { distinctUntilChanged } from 'rxjs';
+import { Pagination } from 'src/app/@shared/interface/pagination'
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
+})
+export class PaginationComponent {
+
+  @Input({ required: true }) items: number;
+  @Input() activePage: number = 1;
+  @Input() perPage: number = 100;
+  @Input() size: string = 'md';
+
+  @Output() onPageChange = new EventEmitter<Pagination>();;
+
+  config: Pagination = null;
+  perPageCtrl: FormControl;
+
+  constructor() {
+    this.perPageCtrl = new FormControl(this.perPage);
+
+    this.perPageCtrl.valueChanges.pipe(distinctUntilChanged()).subscribe((val: number) => {
+      this.perPage = val;
+      this.pageChange(1);
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const items = changes['items'].currentValue;
+    if (items) {
+      this.pageChange(this.config?.activePage);
+    }
+  }
+
+  pageChange(page: number = 1): void {
+    if ((page < 1) || (page > this.config?.totalPages) || (page === this.config?.activePage)) {
+      return;
+    }
+
+    this.config = this.getPaginator(this.items, page, this.perPage);
+    this.onPageChange.emit(this.config);
+  }
+
+  getPaginator(totalItems: number, activePage: number, perPage: number): Pagination {
+    const totalPages: number = Math.ceil(totalItems / perPage);
+    let startPage: number, endPage: number, isStartEllipsesShow: boolean, isEndEllipsesShow: boolean;
+
+    if (totalPages <= 5) {
+      startPage = 1;
+      endPage = totalPages;
+      isStartEllipsesShow = false;
+      isEndEllipsesShow = false;
+    } else if (activePage <= 3) {
+      startPage = 1;
+      endPage = 4;
+      isStartEllipsesShow = false;
+      isEndEllipsesShow = true;
+    } else if ((activePage + 2) >= totalPages) {
+      startPage = totalPages - 3;
+      endPage = totalPages;
+      isStartEllipsesShow = true;
+      isEndEllipsesShow = false;
+    } else {
+      startPage = activePage - 1;
+      endPage = activePage + 1;
+      isStartEllipsesShow = true;
+      isEndEllipsesShow = true;
+    }
+
+    return {
+      activePage: activePage,
+      perPage: perPage,
+      totalItems: totalItems,
+      totalPages: totalPages,
+      isStartEllipsesShow: isStartEllipsesShow,
+      isEndEllipsesShow: isEndEllipsesShow,
+      pages: startPage <= endPage ? Array(endPage - startPage + 1).fill(0).map((x, i) => startPage + i) : []
+    };
+  }
+}
--- src/app/views/users/user.module.ts
+++ src/app/views/users/user.module.ts
@@ -3,44 +3,18 @@ import {
   NO_ERRORS_SCHEMA,
   NgModule,
 } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-import {
-  BadgeModule,
-  ButtonModule,
-  CardModule,
-  FormModule,
-  GridModule,
-  ModalModule,
-  PaginationModule,
-  ProgressModule,
-  TableModule,
-  ToastModule,
-} from '@coreui/angular';
-import { IconModule } from '@coreui/icons-angular';
+import { SharedModule } from 'src/app/@shared/shared.module'
 import { UserComponent } from './user.component';
 import { EditUserDialogComponent } from './edit-user-dialog/edit-user-dialog.component';
 import { DeleteDialogComponent } from './delete-confirmation-dialog/delete-dialog.component';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [UserComponent, EditUserDialogComponent, DeleteDialogComponent],
   imports: [
-    CommonModule,
-    UserRoutingModule,
-    CardModule,
-    ButtonModule,
-    GridModule,
-    IconModule,
-    FormModule,
-    TableModule,
-    BadgeModule,
-    PaginationModule,
-    ModalModule,
-    FormsModule,
-    ToastModule,
-    ProgressModule,
+    SharedModule,
+    UserRoutingModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
 })
