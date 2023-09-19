1ca9f0c4e006c9f7f704a9c2704dca9a47e736af
Swapnil Opash <sra.opash@gmail.com>
2023-09-19 18:53:39 +0530

    env changed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
   hmr: false,
-  serverUrl2: 'https://freedom-api.opash.in/api/v1/',
-  serverUrl: 'http://localhost:8080/api/v1/',
+  serverUrl: 'https://freedom-api.opash.in/api/v1/',
+  serverUrl2: 'http://localhost:8080/api/v1/',
   socketUrl: 'https://freedom-api.opash.in/',
   socketUrl2: 'http://localhost:8080/',
   imgUrl: 'root/storage/uploads',
